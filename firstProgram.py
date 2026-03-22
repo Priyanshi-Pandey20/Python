9c2ac8f6a697b9a9cca828e8e1e567e95af8f97f
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-03-22 19:51:59 +0530

    hi

--- firstProgram.py
+++ firstProgram.py
@@ -1,3 +1,6 @@
 print("Hello World :)")
-name = input("Enter your name : ")
-print(name)
+radius = float(input("Enter the radius : "))
+pie = 3.14
+area = pie * radius * radius
+print("The area of circle is: ")
+print(area)
