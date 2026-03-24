0d9829454a4a3ea5cbb979211cc8ffaa9fbbf7f3
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-03-24 20:28:34 +0530

    explicit conversion

--- explicit.py
+++ explicit.py
@@ -2,3 +2,10 @@ num = input("Enter a value: ")
 convertedValue = float(num)
 print("Original value:",num,"Data Type",type(num))
 print("Original value:",convertedValue,"Data Type",type(convertedValue))
+
+
+totalBill = int(input("enter the total amount: "))
+friendsNo = int(input("Enter the no. of friends: "))
+splitAmount = totalBill/friendsNo
+print("Bill will be : ", splitAmount)
+
