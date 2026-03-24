210d1d3b6baf87358089d24e43b4a581c8e33002
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-03-24 19:46:42 +0530

    explicit conversion

---
+++ explicit.py
@@ -0,0 +1,4 @@
+num = input("Enter a value: ")
+convertedValue = float(num)
+print("Original value:",num,"Data Type",type(num))
+print("Original value:",convertedValue,"Data Type",type(convertedValue))
