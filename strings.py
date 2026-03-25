721875ec7dbfbb5f582fdd743bf0940a9d960cee
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-03-25 20:10:00 +0530

    strings

--- strings.py
+++ strings.py
@@ -1,13 +1,26 @@
-name = "priyanshi"
-str1 = name[0]
-str2 = name[8]
-length = len(name)
-print("First letter of my name :",str1)
-print("Last letter of my name : ", str2)
-print("Length of my name : ",length)
+# name = "priyanshi"
+# str1 = name[0]
+# str2 = name[8]
+# length = len(name)
+# print("First letter of my name :",str1)
+# print("Last letter of my name : ", str2)
+# print("Length of my name : ",length)
 
 
-food = str(input("Enter your fav food name : "))#halwa
-str1 = food[1:4]
-print(str1)
+# food = str(input("Enter your fav food name : "))#halwa
+# mid = len(food)//2
+# str1 = food[mid-1:mid+2]
+
+# print(str1)
+# print(food.upper())
+# print(food.lower())
+# print(food.count("a"))
+# print(food.replace("halwa","pasta"))
+
+
+input = str(input("Enter the sentence: "))#halwa
+print(input.lower())
+print(input.replace(" ","_"))
+
+
 
