525f64ae0c935fc31c93f1c55ecd9a8505fa6e1f
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-03-25 19:27:20 +0530

    string

---
+++ strings.py
@@ -0,0 +1,8 @@
+name = "priyanshi"
+str1 = name[0]
+str2 = name[8]
+length = len(name)
+print("First letter of my name :",str1)
+print("Last letter of my name : ", str2)
+print("Length of my name : ",length)
+
