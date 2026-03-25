5d8ca10bc9c4d0471747b3892b05d90956cb1631
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-03-25 20:46:09 +0530

    conditional statements

---
+++ conditionalstatements.py
@@ -0,0 +1,6 @@
+marks = int(input("Enter the number: "))
+
+if(marks>0):
+    print("f{marks} is positive")
+else:
+    print("f{marks} is negative")
--- emojiConverter.py
+++ emojiConverter.py
@@ -1,7 +1,11 @@
-msg = input("Enter your message : ")
+# STRINGS ARE IMMUTABLE
+# WE CAN REPLACE THE STRING WITH OTHER STRING USING REPLACE KEYWORD
+
+
+msg = input("Enter your message : ")   
 msg = msg.replace(":)","😊")
 msg = msg.replace(":(","😔")
 msg = msg.replace(":D","😄")
 msg = msg.replace(";)","😜")
 print(msg)
-                  
+
