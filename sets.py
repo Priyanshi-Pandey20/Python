f74f247302f5ffbaeead63a26a8d97e00bdbdb3e
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-03-28 20:25:24 +0530

    converting list into sets

--- sets.py
+++ sets.py
@@ -1,2 +1,9 @@
 food = {"chat","gol gappe","aloo chaap","momo"}
 print(type(food))
+food.add("pattie")
+print(food)
+
+programmingList = ["pyhton","java","c++","sql","python"]
+print(type(programmingList))
+programmingSet = set(programmingList)
+print(type(programmingSet))
