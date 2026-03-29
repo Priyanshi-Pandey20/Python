734069f5da99b3dcabc37ece9970953d5f1a8ccd
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-03-29 21:21:32 +0530

    loops

--- loopBasic.py
+++ loopBasic.py
@@ -1,44 +1,45 @@
 
-even = 2
+# even = 2
 
-while even <= 50:
-    if(even%2 == 0):
-     print(even)
+# while even <= 50:
+#     if(even%2 == 0):
+#      print(even)
 
-    even += 1
+#     even += 1
 
-num = int(input("Enter the natural no. "))
-naturalno = 0
-i = 0
-while i<=num:
-   naturalno = naturalno + i
-   i += 1
+# num = int(input("Enter the natural no. "))
+# naturalno = 0
+# i = 0
+# while i<=num:
+#    naturalno = naturalno + i
+#    i += 1
 
-print(naturalno)
+# print(naturalno)
 
 
-n= 1
-while n<=4:
-   print("*" * n)
-   n += 1
+# n= 1
+# while n<=4:
+#    print("*" * n)
+#    n += 1
 
-foodList = ["cakes","mango","choclates"]
-for item in foodList:
-   print(item)
+# foodList = ["cakes","mango","choclates"]
+# for item in foodList:
+#    print(item)
 
-for item in range(1,8,1):
-   print(item)   
+# for item in range(1,8,1):
+#    print(item)   
 
-for num in range(2,20,2):
-   print(num)  
+ 
 
 
 i = 1
 while(i<=50):
    if(i%5 == 0):
       print("Priyanshi Pandey")
+   else:  
+      print(i)     
    i += 1
-print(i)   
+    
     
    
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,32 @@
-num = 1
-# while num<=10:
-#     print("Hello")
-#     num = num+1
 
-# num1 = 10  
+# even = 2
+
+# while even <= 50:
+#     if(even%2 == 0):
+#      print(even)
+
+#     even += 1
+
+# num = int(input("Enter the natural no. "))
+# naturalno = 0
+# i = 0
+# while i<=num:
+#    naturalno = naturalno + i
+#    i += 1
+
+# print(naturalno)
+
+
+# n= 1
+# while n<=4:
+#    print("*" * n)
+#    n += 1
+
+# foodList = ["cakes","mango","choclates"]
+# for item in foodList:
+#    print(item)
+
+# for item in range(1,8,1):
+#    print(item)   
+
+ 
