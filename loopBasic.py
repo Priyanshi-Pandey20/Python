0a8779855a98ad4fb4f19536242baa4e17cabf4b
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-03-29 20:04:28 +0530

    loop

--- loopBasic.py
+++ loopBasic.py
@@ -1,5 +1,10 @@
-num = 1
-while num<=10:
-    print("Hello")
-    num = num+1
-    
+# num = 1
+# while num<=10:
+#     print("Hello")
+#     num = num+1
+
+num1 = 10   
+
+while num1 >= 1:
+    print(num1)
+    num1 = num1-1
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-😜
+num = 1
+# while num<=10:
+#     print("Hello")
+#     num = num+1
+
+# num1 = 10  
