0ba769803a797a4b891f103b0847126952c689ae
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-04-01 11:29:02 +0530

    functionBasics

--- functionParam.py
+++ functionParam.py
@@ -9,3 +9,13 @@ average(7,10)
 def show_age(name,age):
     print(f"{name} is {age} years old")
 show_age("Priyanshi",25)    
+
+
+def sum(a,b):
+    addno = a+b
+    diffno = a-b
+    print(addno)
+    print(diffno)
+
+sum(5,5)
+sum(10,5)
