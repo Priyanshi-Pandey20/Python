16479e51acefd5a5c0da83fc90a0678c39c411c1
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-04-03 19:36:41 +0530

    file

--- fileBasics.py
+++ fileBasics.py
@@ -2,7 +2,7 @@ file = open("mast.txt","r")
 data = file.read()   #READING THE ENTIRE FILE 
 dataFile = data.lower()
 
-if "live" in dataFile:
+if "live" in dataFile: #CHECK LIVE KEYWORD EXISTS IN A FILE OR NOT
     print("Yes Live word is present in file")
 
 file = open("report.txt","w") #WRITING IN A FILE 
@@ -10,6 +10,12 @@ file.write("Hi I am learning Python")
 
 file = open("report.txt","a") #APPENDING A FILE 
 file.write("Hi I am learning Python")
+file.close()
+
+with open("mast.txt","r") as f:  #WITH KEYWORD 
+    data = f.read()
+    print("file data",data)
+
 
 
 
