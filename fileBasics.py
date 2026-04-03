cc591099fe1a575dbeab58d5a1cad23ebdfe7c4d
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-04-03 19:25:49 +0530

    file operations

--- fileBasics.py
+++ fileBasics.py
@@ -1,4 +1,11 @@
 file = open("mast.txt","r")
 data = file.read()
+dataFile = data.lower()
 
-print("Data of the file is : ",data)
+if "live" in dataFile:
+    print("Yes Live word is present in file")
+
+file = open("report.txt","a")
+file.write("Hi I am learning Python")
+
+
