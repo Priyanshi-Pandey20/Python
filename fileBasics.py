c1c8e690ab361a8e25b0dee2bdfbf8b789b2d60d
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-04-03 19:48:55 +0530

    file

--- fileBasics.py
+++ fileBasics.py
@@ -12,9 +12,27 @@ file = open("report.txt","a") #APPENDING A FILE
 file.write("Hi I am learning Python")
 file.close()
 
-with open("mast.txt","r") as f:  #WITH KEYWORD 
-    data = f.read()
-    print("file data",data)
+# with open("mast.txt","r") as f:  #WITH KEYWORD 
+#     data = f.read()
+#     print("file data",data)
+
+# with open("mast.txt","r") as f: #READING THE FILE LINE BY LINE
+#     line1 = f.readline()   
+#     line2 = f.readline()   
+#     line3 = f.readline() 
+#     data= f.read()
+#     print("line1",line1)
+#     print("line2",line2)
+#     print("line3",line3)  
+#     print("file data",data)
+
+with open("mast.txt","r") as f: #READING ALL THE LINES IN ONE GO 
+    line = f.readlines()
+    print(line)
+
+    
+
+
 
 
 
