36ce1b42b165ee9efd333a69ae20929f0f9c3bed
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-04-03 19:31:11 +0530

    file

--- fileBasics.py
+++ fileBasics.py
@@ -1,11 +1,16 @@
 file = open("mast.txt","r")
-data = file.read()
+data = file.read()   #READING THE ENTIRE FILE 
 dataFile = data.lower()
 
 if "live" in dataFile:
     print("Yes Live word is present in file")
 
-file = open("report.txt","a")
+file = open("report.txt","w") #WRITING IN A FILE 
 file.write("Hi I am learning Python")
+
+file = open("report.txt","a") #APPENDING A FILE 
+file.write("Hi I am learning Python")
+
+
 
 
