67487c386b8439fb7c1d5bddbd7bf888e5cc6211
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-04-04 19:23:45 +0530

    instance attribute

--- oops.py
+++ oops.py
@@ -11,5 +11,20 @@ print(bike.milege)
 aeroplane = Vehicle()
 print(aeroplane.petrol)
 
+class Car:
+    brand="swift"
+
+obj = Car()
+print(obj.brand)
+
+#INSTANCE ATTRIBUTE
+class Student:
+    college="ABC College"
+    def __init__(self):
+        print("Wheneve a new object is created I am called automatically")
+student1 = Student()
+student2 = Student()        
+
+
 
 
