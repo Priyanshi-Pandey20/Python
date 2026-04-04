0f63b564cb2e6733d9db826e6814157ffebe3576
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-04-04 19:42:21 +0530

    constructor

--- oops.py
+++ oops.py
@@ -20,10 +20,18 @@ print(obj.brand)
 #INSTANCE ATTRIBUTE
 class Student:
     college="ABC College"
-    def __init__(self):
+    def __init__(self,name,course):#the object for which init is called in passed here (IT IS COMPULSORY TO PASS IT )
         print("Wheneve a new object is created I am called automatically")
-student1 = Student()
-student2 = Student()        
+        print(name)
+        print(course)
+student1 = Student("Sneha","Communication")#init method will be called
+student2 = Student("Khushi","B.Tech")  
+
+
+
+
+
+
 
 
 
