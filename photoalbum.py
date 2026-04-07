b665bfff36705b3ca800416e38531f8ba2efc9a0
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-04-07 20:47:25 +0530

    photoalbum

---
+++ photoalbum.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+import time
+from PIL import Image,ImageTk 
+
+#Main Application window
+root = tk.Tk()
+root.title("Photo SLideshow Album")
+root.geometry("900x900")
+
+#List of Image Paths
+image_paths=[
+    r"\Users\Dell\OneDrive\Desktop\Album\img1.avif",
+    r"\Users\Dell\OneDrive\Desktop\Album\img2.webp",
+    r"\Users\Dell\OneDrive\Desktop\Album\img1.avif",
+    r"\Users\Dell\OneDrive\Desktop\Album\img1.jpg", 
+    r"\Users\Dell\OneDrive\Desktop\Album\img1.avif",
+    r"\Users\Dell\OneDrive\Desktop\Album\img1.jpg",
+    "",
+]
+image_size = (700,700)
+images=[]
+for path in image_paths:
+    img= Image.open(path)
+    img=img.resize(image_size)
+    images.append(img) #Adding each image in the list
+
+#Convert PIL Images into Tkinter Compatible Image
+final_images=[]
+for img in images:
+    photo = ImageTk.PhotoImage(img) 
+    final_images.append(photo)
+
+#Label widget to keep photo
+image_label = tk.Label(root)
+image_label.pack(pady=30)
+
+#Slidesshow Function
+def start_slideshow():
+    for photo in final_images:
+        image_label.config(image=photo)
+        image_label.image = photo
+        root.update()
+        time.sleep(2)
+
+#Button
+play_button = tk.Button(
+    root,
+    text = "Play the Slideshow"
+    font=("Arial",17)
+    command=start_slideshow
+)
+play_button.pack(pady=40)
+
+
+
+
+
+
+
+
